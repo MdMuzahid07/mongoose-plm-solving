6eab985163f4a5b52a43a34fcdfb0e8d8906be72
MdMuzahid07 <mdmuzahid7396@gmail.com>
2024-11-02 19:55:11 +0600

    plm 4 solved

--- index.js
+++ index.js
@@ -60,4 +60,31 @@ const composedFunction = (num) => addFive(double(square(num)));
 
 
 const output = composedFunction(5);
-console.log(output); 
+// console.log(output);
+
+
+
+
+
+// 4.Task: Sorting Objects
+
+// Create an array of objects representing cars with properties like make, model, and year.Write a function to sort the array of cars by the year of manufacture in ascending order.Print the sorted array.
+
+
+
+
+const cars = [
+    { make: "Mercies", model: "fx-11", year: 2018 },
+    { make: "BMW", model: "hk-2", year: 2020 },
+    { make: "Ford", model: "Mustang", year: 2015 },
+    { make: "Tesla", model: "X-1", year: 2022 },
+];
+
+const sortCars = (carsArray) => {
+    return carsArray.sort((a, b) => a.year - b.year);
+};
+
+
+const sortCarsByYear = sortCars(cars);
+
+// console.log(sortCarsByYear);
